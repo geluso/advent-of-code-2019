82ba20a0b3ddbf2dbac0b89cd8d0381ad6092458
Steve Geluso <stevegeluso@gmail.com>
2019-12-02 16:29:42 -0800

    removing target

--- 00-rust-sandbox/target/debug/deps/minigrep-db0ec3abcbb9387f.d
+++
@@ -1,5 +0,0 @@
-/Users/moonmayor/Code/advent-of-code-2019/00-rust-sandbox/target/debug/deps/minigrep-db0ec3abcbb9387f: src/main.rs
-
-/Users/moonmayor/Code/advent-of-code-2019/00-rust-sandbox/target/debug/deps/minigrep-db0ec3abcbb9387f.d: src/main.rs
-
-src/main.rs:
--- 00-rust-sandbox/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/Users/moonmayor/Code/advent-of-code-2019/00-rust-sandbox/target/debug/minigrep.dSYM: /Users/moonmayor/Code/advent-of-code-2019/00-rust-sandbox/src/main.rs
