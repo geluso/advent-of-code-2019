4ed3c931a82c2e35649bb4430f1a6c9674e9f74c
Steve Geluso <stevegeluso@gmail.com>
2019-12-03 17:49:46 -0800

    reading points

--- 03-crossed-wires/src/main.rs
+++ 03-crossed-wires/src/main.rs
@@ -14,40 +14,58 @@ fn main() -> io::Result<()> {
     let path1: Vec<&str> = paths[0].split(",").collect();
     let path2: Vec<&str> = paths[1].split(",").collect();
 
-    println!("path1 {:?}", path1);
-    println!("path2 {:?}", path2);
+    println!("path1 {:?}", &path1);
+    println!("path2 {:?}", &path2);
 
-    let mut path1_points = HashSet::new();
-    let mut path2_points = HashSet::new();
-
-    path1_points.insert("(0,0");
-    path2_points.insert("(0,0");
-
-    let mut xx: isize = 0;
-    let mut yy: isize = 0;
-    for direction in path1 {
-        // why can't I assign directly back to xx and yy?
-        //let (aa, bb) = point_to_point(xx, yy, direction);
-        //xx = aa;
-        //yy = bb;
-        (xx, yy) = point_to_point(xx, yy, direction);
-        println!("{} {}", xx, yy);
-    }
+    let points1 = path_points(&path1);
+    let points2 = path_points(&path2);
 
     Ok(())
 }
 
-fn point_to_point(xx: isize, yy: isize, direction: &str) -> (isize, isize) {
-    let letter = direction.chars().next().unwrap();
-    if letter == 'R' {
-        return (xx + 1, yy);
-    } else if letter == 'L' {
-        return (xx - 1, yy);
-    } else if letter == 'U' {
-        return (xx, yy + 1);
-    } else if letter == 'D' {
-        return (xx, yy - 1);
+fn path_points(path: &Vec<&str> ) -> HashSet<String> {
+    let mut points: HashSet<String> = HashSet::new();
+    points.insert("(0,0)".to_string());
+
+    let mut xx: isize = 0;
+    let mut yy: isize = 0;
+    for direction in path {
+        println!("{}", direction);
+        let (aa, bb) = point_to_point(&mut points, direction, xx, yy);
+        xx = aa;
+        yy = bb;
     }
+
+    return points;
+}
+
+fn point_to_point(path: &mut HashSet<String>, direction: &str, mut xx: isize, mut yy: isize) -> (isize, isize) {
+    let mut chars = direction.chars();
+    let letter = chars.next().unwrap();
+    let nn = chars.as_str().parse::<u32>().unwrap();
+
+    let mut dx = 0;
+    let mut dy = 0;
+
+    if letter == 'R' {
+        dx = 1;
+    } else if letter == 'L' {
+        dx = -1;
+    } else if letter == 'U' {
+        dy = 1;
+    } else if letter == 'D' {
+        dy = -1;
+    }
+
+    for _ in 1..=nn {
+        xx += dx;
+        yy += dy;
+
+        let coord = format!("({},{})", xx, yy);
+        println!("{}", coord);
+        path.insert(coord);
+    }
+
 
     (xx, yy)
 }
