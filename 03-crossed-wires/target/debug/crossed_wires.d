ec965c74a26b686ee1d14a12e8f7c06eb6dc13c1
Steve Geluso <stevegeluso@gmail.com>
2019-12-03 17:10:24 -0800

    removing target

--- 03-crossed-wires/target/debug/crossed_wires.d
+++
@@ -1,1 +0,0 @@
-/Users/moonmayor/Code/advent-of-code-2019/03-crossed-wires/target/debug/crossed_wires.dSYM: /Users/moonmayor/Code/advent-of-code-2019/03-crossed-wires/src/main.rs
--- 03-crossed-wires/target/debug/deps/crossed_wires-75e149661c721283.d
+++
@@ -1,5 +0,0 @@
-/Users/moonmayor/Code/advent-of-code-2019/03-crossed-wires/target/debug/deps/crossed_wires-75e149661c721283: src/main.rs
-
-/Users/moonmayor/Code/advent-of-code-2019/03-crossed-wires/target/debug/deps/crossed_wires-75e149661c721283.d: src/main.rs
-
-src/main.rs:
