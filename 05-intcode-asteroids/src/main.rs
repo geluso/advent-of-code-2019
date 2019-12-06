6c33217a9f11c6f3941dedc4103e7398b92dbb42
Steve Geluso <stevegeluso@gmail.com>
2019-12-05 16:32:50 -0800

    mild cleanup

--- 05-intcode-asteroids/src/main.rs
+++ 05-intcode-asteroids/src/main.rs
@@ -2,33 +2,12 @@ use std::env;
 use std::fs;
 use std::io::{self, BufRead};
 
-// 99 means that the program is finished and should immediately halt. Encountering an unknown
-// opcode means something went wrong.
-
-// Opcode 1 adds together numbers read from two positions and stores the result in a third
-// position. The three integers immediately after the opcode tell you these three positions - the
-// first two indicate the positions from which you should read the input values, and the third
-// indicates the position at which the output should be stored.
-// 
-// For example, if your Intcode computer encounters 1,10,20,30, it should read the values at
-// positions 10 and 20, add those values, and then overwrite the value at position 30 with their
-// sum.
-// 
-// Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding
-// them. Again, the three integers after the opcode indicate where the inputs and outputs are, not
-// their values.
-// 
-// Once you're done processing an opcode, move to the next one by stepping forward 4 positions.
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let filename = parse_config(&args);
 
     let contents = fs::read_to_string(filename)?;
     let codes: Vec<&str> = contents.trim().split(",").collect();
-
-    // print a vector with {:?}
-    // pretty print a vector with {:#?}
-    // println!("codes: {:?}", codes);
 
     let mut strip = vec![];
     for code in &codes {
@@ -37,22 +16,6 @@ fn main() -> io::Result<()> {
     }
 
     compute(&mut strip);
-
-    // old searching code
-    //
-    // let seeking = 19690720;
-    // for noun in 0..=99 {
-    //     for verb in 0..=99 {
-    //         strip[1] = noun;
-    //         strip[2] = verb;
-
-    //         let mut fresh_strip = strip.clone();
-    //         let result = compute(&mut fresh_strip);
-    //         if result == seeking {
-    //             println!("{} {}", noun, verb);                       
-    //         }
-    //     }
-    // }
 
     Ok(())
 }
